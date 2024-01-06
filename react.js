a9bd84dc790c0be5d4025c457cb7d0d8d9feecd1
cobrakai <43102547+lagerking0@users.noreply.github.com>
2024-01-06 17:00:57 +0800

    Update react.js

--- react.js
+++ react.js
@@ -1,8 +1,17 @@
 import { Provider, LikeButton } from "@lyket/react";
 
-<Provider apiKey="pt_8f1f26e50d58be724353a8609f099d">
+<Provider
+  apiKey="pt_8f1f26e50d58be724353a8609f099d"
+  theme={{
+    colors: {
+      background: "#b8fff3",
+      text: "violet",
+      primary: "rgba(255, 224, 138, 0.4)"
+    }
+  }}
+>
   <LikeButton
-    namespace="testing-react"
-    id="everybody-like-now"
+    namespace="my-blog-post"
+    id="how-to-beat-me-at-chess"
   />
 </Provider>
