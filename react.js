1d76efa3d2c4d4b72722e2070cde2db9d3259a39
cobrakai <43102547+lagerking0@users.noreply.github.com>
2024-01-06 16:40:53 +0800

    Create react.js

---
+++ react.js
@@ -0,0 +1,8 @@
+import { Provider, LikeButton } from "@lyket/react";
+
+<Provider apiKey="pt_8f1f26e50d58be724353a8609f099d">
+  <LikeButton
+    namespace="testing-react"
+    id="everybody-like-now"
+  />
+</Provider>
